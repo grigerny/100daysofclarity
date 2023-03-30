d761460d9d024ad3317c5daf70bbcbaa48067fa9
Gary Riger <gary@bklyn.co>
2023-03-30 09:51:55 -0400

    Day 64

--- tests/nft-simple_test.ts
+++ tests/nft-simple_test.ts
@@ -1,26 +1,24 @@
-
-import { Clarinet, Tx, Chain, Account, types } from 'https://deno.land/x/clarinet@v1.3.1/index.ts';
+import { Clarinet, Tx, Chain, Account, Contract, types } from 'https://deno.land/x/clarinet@v1.3.1/index.ts';
 import { assertEquals } from 'https://deno.land/std@0.170.0/testing/asserts.ts';
 
 Clarinet.test({
-    name: "Ensure that <...>",
-    async fn(chain: Chain, accounts: Map<string, Account>) {
-        let block = chain.mineBlock([
-            /*
-             * Add transactions with:
-             * Tx.contractCall(...)
-            */
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 2);
+         name: "Ensure that the right amount of STX (NFT Price) is transferred",
+         async fn(chain: Chain, accounts: Map<string, Account>, Contracts: Map<string, Contract>) {
+            let deployer = accounts.get("deployer")!;
+            let wallet_1 = accounts.get("wallet_1")!;
 
-        block = chain.mineBlock([
-            /*
-             * Add transactions with:
-             * Tx.contractCall(...)
-            */
-        ]);
-        assertEquals(block.receipts.length, 0);
-        assertEquals(block.height, 3);
-    },
-});
+            let block = chain.mineBlock([
+                Tx.contractCall("nft-simple", "mint", [], wallet_1.address)
+            ]);
+
+            block.receipts[0].result.expectOk().expectBool(true);
+
+            console.log(block.receipts[0].events)
+            block.receipts[0].events.expectSTXTransferEvent(
+                10000000,
+                wallet_1.address,
+                deployer.address
+            )
+           
+         }
+    })
